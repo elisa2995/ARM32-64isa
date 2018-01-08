8d6f0605a74b61295ef488d476a6f0085ed13258
elisa2995 <elisa2995@yahoo.com>
2018-01-08 11:06:16 +0000

    Started compression

--- sha2.s
+++ sha2.s
@@ -7,7 +7,7 @@
 .equ INPUT_LENGTH_B, 5 /*byte*/
 .equ INPUT_LENGTH_W, 2
 .balign 4
-h: .word 0x6a09e667,0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+hash: .word 0x6a09e667,0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
 .balign 4
 k: .word    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,0x06ca6351, 0x14292967,0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,0x5b9cca4f, 0x682e6ff3,0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 
@@ -15,8 +15,6 @@ input_b: .byte 0x01, 0x02,0x03,0x04, 0x05
 input_w: .word 0,0
 processed: .word 0, 0, 10, 10,0, 0, 10, 10,0, 0, 10, 10,0, 0, 10, 10
 message: .word   0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0,  0, 0,0, 0,0, 0,0, 0
-s0: .word 0
-s1: .word 0
 a: .word 0
 b: .word 0
 c: .word 0
@@ -39,8 +37,8 @@ main:
 	bl fillEnd
 	
 	/*COMPRESSION*/
-	
 	bl processChunks
+	
 	
 breakpoint:
 	pop {lr}
@@ -60,25 +58,25 @@ breakpoint:
  *	input_w[i]=r10;
  * }
  */
- parse:	
+parse:	
  		push {r5, r6, r7, r8, r9, r10, r11, r12}
  		ldr r6, addr_input_b			/*r6->input_b*/
  		ldr r8, addr_input_w			/*r8->input_w*/
- 		mov r5,#0				/*r5=counter i*/
- 		mov r7,#0				/*r7=counter j*/
+ 		mov r5,#0						/*r5=counter i*/
+ 		mov r7,#0						/*r7=counter j*/
 loop_prs_i:
- 		mov r11,#0				/*r11 holds the 4 byte chunk from input*/
- 		mov r7, #0				/*j=0*/
+ 		mov r11,#0						/*r11 holds the 4 byte chunk from input*/
+ 		mov r7, #0						/*j=0*/
 loop_prs_j:
- 		add r9, r7, r5, lsl #2		/*r9<- j+i*WORD*/
- 		cmp r9, #INPUT_LENGTH_B		/*if j+i*WORD == INPUT_LENGTH_B*/
- 		beq end_prs			/*END*/
- 		ldrb r10, [r6, r9]		/*r10<-input_b[j+i*WORD]*/
+ 		add r9, r7, r5, lsl #2			/*r9<- j+i*WORD*/
+ 		cmp r9, #INPUT_LENGTH_B			/*if j+i*WORD == INPUT_LENGTH_B*/
+ 		beq end_prs						/*END*/
+ 		ldrb r10, [r6, r9]				/*r10<-input_b[j+i*WORD]*/
  			
- 		rsb r9, r7, #WORD-1		/*r9<-WORD-j*/		 
- 		lsl r9,#3			/*r9<-(WORD-j)*8*/	
+ 		rsb r9, r7, #WORD-1				/*r9<-WORD-1-j*/		 
+ 		lsl r9,#3						/*r9<-(WORD-1-j)*8*/	
  		lsl r10, r9	
- 		orr r11,r10			/*r11(byte j)=INPUT[j+i*4]*/	
+ 		orr r11,r10						/*r11(byte j)=INPUT[j+i*4]*/	
  				 
  		add r7,r7, #1
  		cmp r7, #WORD
@@ -107,9 +105,9 @@ end_prs:
  */	
 preprocess:	
 		push {r5,r6,r7,r8} 
-		ldr r6, addr_input_w 		/*r6->input*/
-		ldr r8, addr_processed 		/*r8->processed*/
-		mov r5,#0 					/*r5=counter*/
+		ldr r6, addr_input_w 			/*r6->input*/
+		ldr r8, addr_processed 			/*r8->processed*/
+		mov r5,#0 						/*r5=counter*/
 loop_PP:					
 		cmp r5, #INPUT_LENGTH_W
 		ldrlt r7,[r6,r5, lsl #2] 		/*if i<input.length r7<-input[i]*/
@@ -131,16 +129,16 @@ loop_PP:
 startPadding:	
 		
 		push {r5, r6, r7, r8, r9, r10}
-		ldr r8, addr_processed 		/*r8->processed*/
-		mov r7, #INPUT_LENGTH_B+1				
+		ldr r8, addr_processed 			/*r8->processed*/
+		mov r7, #INPUT_LENGTH_B+1		/*r7<- length in byte of input + 0b10000000*/		
 		lsr r7,#2						/*r7<- index of the word that we have to modify*/
 		mov r5, #INPUT_LENGTH_B 		
-		and r5, #0x3					/*r5<- index of the byte in the word that has to be modified(r5 = #INPUT_LENGTH_B MOD 4)*/
-		lsl r5,#3						/*r5<- number of right shifts */
+		and r5, #0b00000011				/*r5= #INPUT_LENGTH_B MOD 4, index of the byte in the word that has to be modified*/
+		lsl r5,#3						/*r5<- r5*8, number of right shifts */
 		ldr r9,[r8,r7, lsl #2]			/*Load the word that has to be modified*/
 		
 		mov r6,#0x10000000		
-		lsr r6, r5						/*Positioning the 1 in the right position*/	
+		lsr r6, r5						/*Positioning the 0x10 the right position*/	
 		orr r9,r6						/*Insert the 1*/
 		str r9,[r8,r7, lsl #2]			/*Store the modified value*/
 		pop {r5, r6, r7, r8, r9, r10}
@@ -160,7 +158,7 @@ startPadding:
 fillEnd:
 		push {r5, r6, r7, r8}
 		movs r5, #INPUT_LENGTH_W			/*if #INPUT_LENGTH_B!=0*/
-		ldrne r6, addr_input_w	
+		ldrne r6, addr_input_w			
 		ldrne r8, addr_processed	
 		ldrne r7,[r6]						/*r7<-input[0]*/
 		strne r7,[r8, #14*4]				/*processed[14]=input_w[0]*/
@@ -175,18 +173,18 @@ fillEnd:
  * processChunks
  * For each chuck(16 words) in which we can divide processed we do functions:
  * 
- *
- *
 */
 processChunks:
 			push {r0, r5, r6, lr}
 			mov r5, #0					/* r5 counter*/
 					
-			
 loop_pChs:	
 			mov r0, r5, lsl #2			/*r0=chunck offset in processed */
+			
 			bl copyProcessed
 			bl fillMessage	
+			bl initAlphabeth
+			bl compress
 			
 			add r5,r5,#16				
 			cmp r5, #PRD_LENGTH_W		
@@ -204,13 +202,11 @@ loop_pChs:
 copyProcessed:
 			push {r5, r6, r7, r8}
 			
-			ldr r7, addr_processed 
-			add r0, r0, r7			/*r0=chunk address*/	
-			
-			mov r5,#0				/*r5 counter*/
 			ldr r6, addr_message
-
-
+			ldr r7, addr_processed 		/*r7=base address of processed*/
+			add r0, r0, r7				/*r0=chunk address*/	
+			
+			mov r5,#0					/*r5 counter*/
 loop_pCh:   
 			ldr r7,[r0,r5, lsl #2]		/*r7<-chunk[i]*/
 			str r7,[r6,r5, lsl #2]		/*message[i]=chunck[i]*/	
@@ -228,12 +224,13 @@ loop_pCh:
  *  s0 := (w[i-15] rightrotate 7) xor (w[i-15] rightrotate 18) xor (w[i-15] rightshift 3)
  *  s1 := (w[i-2] rightrotate 17) xor (w[i-2] rightrotate 19) xor (w[i-2] rightshift 10)
  *  w[i] := w[i-16] + s0 + w[i-7] + s1 	
+ *(w=message)
 */
 fillMessage:
 
 			push {r5, r6, r7, r8, r9, r10, r11, r12}
-			mov r5, #16					/*r5 = i (starts from the end of the chunk and goes to the end of message)*/
-			ldr r6, addr_message		/*w=message*/
+			mov r5, #16					/*r5 = i 16:63 (end of message)*/
+			ldr r6, addr_message		
 loop_fm:	
 			/*r8 <- w[i-15]*/
 			sub r7, r5, #15				/*r7= i-15*/
@@ -257,8 +254,7 @@ loop_fm:
 			lsr r11, r8, #10			/*r11=lsr(w[i-2],10)*/
 			eor r10, r10, r11			/*r10=r0 XOR r11*/
 			
-			/*r9=s0+s1*/
-			add r9, r9, r10
+			add r9, r9, r10				/*r9=s0+s1*/
 			
 			/*r8 <- w[i-16]*/
 			sub r7, r5, #16				/*r7= i-16*/
@@ -281,12 +277,120 @@ loop_fm:
 			
 			pop {r5, r6, r7, r8, r9, r10, r11, r12}
 			bx lr
+/*initAlphabeth
+ *Initializes the letters a to  with hash values (a=hash[0], b=hash[1]....)
+ * 
+*/
+initAlphabeth:
+			push {r0, r1, r5, lr}
+						
+			ldr r0, addr_a
+			mov r1, #0
+			bl initLetter	
 			
-
+			ldr r0, addr_b
+			mov r1, #1
+			bl initLetter
 			
-addr_h: .word h
+			ldr r0, addr_c
+			mov r1, #2
+			bl initLetter	
+			
+			ldr r0, addr_d
+			mov r1, #3
+			bl initLetter
+			
+			ldr r0, addr_e
+			mov r1, #4
+			bl initLetter
+			
+			ldr r0, addr_f
+			mov r1, #5
+			bl initLetter
+			
+			ldr r0, addr_g
+			mov r1, #6
+			bl initLetter
+			
+			ldr r0, addr_m
+			mov r1, #7
+			bl initLetter	
+			
+bp:
+			pop {r0, r1, r5, lr}
+			bx lr
+			
+/*initLetter
+ *
+ *PARAMS:
+ *r0<-letter address
+ *r1<-index of the hash value (i)
+*/
+initLetter:
+			push {r5,r6}
+			
+			ldr r6, addr_hash			/*r6<-hash[0]*/
+			ldr r5, [r6, r1, lsl #2]	/*r5<-hash[i]*/
+			str r5, [r0]				/*letter<-hash[i]*/
+			
+			pop {r5,r6}
+			bx lr
+/*compress
+ *Compression function main loop:
+ *   for i from 0 to 63
+ *       S1 := (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)
+ *       ch := (e and f) xor ((not e) and g)
+ *       temp1 := h + S1 + ch + k[i] + w[i]
+ *       S0 := (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
+ *       maj := (a and b) xor (a and c) xor (b and c)
+ *       temp2 := S0 + maj
+ * 
+ *      h := g
+ *      g := f
+ *      f := e
+ *      e := d + temp1
+ *      d := c
+ *      c := b
+ *      b := a
+ *      a := temp1 + temp2
+ *
+*/
+compress:
+			push {r5, r6, r7, r8, r9, r10, r11, r12}
+			
+			mov r5, #0					/*r5 counter*/
+loop_cpr:	
+			/*r6 := (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)*/	
+			ldr r11, addr_e
+			ldr r11, [r11]				/*r11<-e*/
+			ror r6, r11, #6				/*r6=ror(e,6)*/
+			ror r7, r11, #11			/*r7=ror(e,11)*/
+			eor r6, r6, r7				/*r6=ror(e,6) XOR ror(e,11)*/
+			ror r7,r11, #25				/*r7=ror(e, 25)*/
+			eor r6,r6,r7				/*r6=ror(e,6) XOR ror(e,11)XOR ror(e,11)*/
+			
+			/*ch := (e and f) xor ((not e) and g)*/
+			
+			
+			/*counter update*/
+			add r5,r5,#1
+			cmp r5, #64
+			blt loop_cpr
+			
+			
+			pop {r5, r6, r7, r8, r9, r10, r11, r12}
+			bx lr
+addr_hash: .word hash
 addr_k: .word k
 addr_input_b: .word input_b
 addr_input_w: .word input_w
 addr_processed: .word processed
 addr_message: .word message
+addr_a: .word a
+addr_b: .word b
+addr_c: .word c
+addr_d: .word d
+addr_e: .word e
+addr_f: .word f
+addr_g: .word g
+addr_m: .word m
